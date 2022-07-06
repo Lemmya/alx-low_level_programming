635ad0020221a893adad86ba59fff373a68645fe
Lemmya <otitolayee@gmail.com>
2022-07-06 10:38:58 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#inclide "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @a: the integer to be computed
+ *
+ * Return: absolute value
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+	return (a);
+	else
+		return (a * -1);
+}
