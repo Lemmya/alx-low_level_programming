d42b54147cabf41326183f6a7088abee310c12c5
Lemmya <otitolayee@gmail.com>
2022-07-06 10:01:08 -0700

    trial

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - function to print abc 10 times
+ * print_alphabet_x10 - prints the alphabet 10 times
  *
- * Return: 0
+ * Return: void
  */
 
 void print_alphabet_x10(void)
